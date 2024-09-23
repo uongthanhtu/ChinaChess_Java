f161c9a381a292c382b1abdcb9b28ef703579cff
Tusry Uong <uongthanhtu5@gmail.com>
2024-09-24 00:55:49 +0700

    Done Elephant Role move

--- src/src/chess/Elephant.java
+++ src/src/chess/Elephant.java
@@ -15,8 +15,44 @@ public class Elephant extends Piece{
     }
 
     @Override
-    public boolean isValidMove(int newMoveX, int newMoveY, Piece[][] boards) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean isValidMove(int newMoveX, int newMoveY, Piece[][] board) {
+        if(newMoveX == x || newMoveY == y){
+            return false;
+        }
+        Piece piece = board[x][y];
+        if((newMoveX == x + 2 || newMoveX == x - 2) && (newMoveY == y + 2 || newMoveY == y - 2)){
+            if(newMoveY == 0 || newMoveY == 2 || newMoveY == 6 || newMoveY == 8 || newMoveY == 4){
+                if(piece.getColor().equalsIgnoreCase("Den")){
+                    if(newMoveX == 0 || newMoveX == 2 || newMoveX == 4){
+                        if(board[newMoveX][newMoveY] != null && board[newMoveX][newMoveY].getColor().equalsIgnoreCase("Den")) {
+                            return false;
+                        }
+                        int checkBlockX = (newMoveX + x) / 2;
+                        int checkBlockY = (newMoveY + y) / 2;
+                        if(board[checkBlockX][checkBlockY] != null){
+                            return false;
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+                if(piece.getColor().equalsIgnoreCase("Do")){
+                    if(newMoveX == 5 || newMoveX == 7 || newMoveX == 9){
+                        if(board[newMoveX][newMoveY] != null && board[newMoveX][newMoveY].getColor().equalsIgnoreCase("Do")) {
+                            return false;
+                        }
+                        int checkBlockX = (newMoveX + x) / 2;
+                        int checkBlockY = (newMoveY + y) / 2;
+                        if(board[checkBlockX][checkBlockY] != null){
+                            return false;
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
